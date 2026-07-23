f005bf0a8ce39a71f12af3073d1689d64615cd6e
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-23 19:22:47 -0300

    Corrigindo PrismaService para versao 7 sem datasources

--- boo-api/prisma/prisma.service.ts
+++ boo-api/prisma/prisma.service.ts
@@ -3,6 +3,10 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super(); // O Prisma v7 lê automaticamente a DATABASE_URL do arquivo .env
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
