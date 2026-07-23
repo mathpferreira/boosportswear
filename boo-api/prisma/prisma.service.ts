c5e78b5e5d0b948fd5e205d16290823503c1fd67
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-23 19:52:18 -0300

    Atualizando schema.prisma com a coluna nome

--- boo-api/prisma/prisma.service.ts
+++ boo-api/prisma/prisma.service.ts
@@ -3,13 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    super({
-      // Força o Prisma v7 a aceitar a URL direta do .env sem exigir driver adapter externo
-      omit: {},
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
