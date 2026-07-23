7eaa7b1ea7e32c931b213e81f442da2891604865
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-23 18:55:17 -0300

    Corrigindo caminho do import do PrismaModule

--- boo-api/src/auth/auth.module.ts
+++ boo-api/src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [
+    PrismaModule, // << ADICIONADO AQUI NOS IMPORTS
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'boo_secret_key_123',
       signOptions: { expiresIn: '7d' },
