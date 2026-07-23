2d15485507b4061bd134605a5eb598dd426e2e64
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-23 18:57:31 -0300

    Corrigindo injecao do PrismaService no AuthModule

--- boo-api/src/auth/auth.module.ts
+++ boo-api/src/auth/auth.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from '../../prisma/prisma.module';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   imports: [
-    PrismaModule, // << ADICIONADO AQUI NOS IMPORTS
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'boo_secret_key_123',
       signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
   exports: [AuthService],
 })
 export class AuthModule {}
