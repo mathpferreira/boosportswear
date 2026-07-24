d621a8bb3f33c4510b7195c0c118657e121fc2b1
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-24 13:04:40 -0300

    Removendo prefixo global para o Nginx funcionar

--- boo-api/src/main.ts
+++ boo-api/src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  // Tiramos o bodyParser: false daqui e deixamos o padrão
   const app = await NestFactory.create(AppModule);
   
   app.enableCors();
-  app.setGlobalPrefix('api');
+  
+  // ❌ APAGUE OU COMENTE ESTA LINHA:
+  // app.setGlobalPrefix('api'); 
 
-  // Forçando os 50MB com o pacote nativo body-parser
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
