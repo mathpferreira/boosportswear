39460487d7daf21fdc04f65fa2145e938d01c0c2
Matheus Ferreira <mxthferreira@gmail.com>
2026-07-24 12:28:58 -0300

    feat: painel admin finalizado com alertas e dashboard 3

--- boo-api/src/main.ts
+++ boo-api/src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json, urlencoded } from 'express'; // <-- Importe isso
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Aumente o limite para 50MB
+  // Limites gigantes para a foto
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   
   app.enableCors();
+
+  // A LINHA MÁGICA QUE RESOLVE O 404:
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
